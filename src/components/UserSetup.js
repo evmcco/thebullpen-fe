8f3ac2ca5db0d1a1f2bde718e22653f581967b64
Evan McCoy <evan.mccoy@gmail.com>
2021-02-13 08:12:47 -0500

    checking if username exists during username setup, saving username to DB

--- src/components/UserSetup.js
+++ src/components/UserSetup.js
@@ -12,6 +12,7 @@ const UserSetup = () => {
   const [userMetadata, setUserMetadata] = useState({})
   const [usernameInput, setUsernameInput] = useState('')
   const [usernameSaved, setusernameSaved] = useState(false)
+  const [usernameExists, setUsernameExists] = useState()
 
   const useStyles = makeStyles((theme) => ({
     root: {
@@ -30,24 +31,50 @@ const UserSetup = () => {
     if (user["https://thebullpen.app/user/user_metadata"].username) {
       setUserMetadata(user["https://thebullpen.app/user/user_metadata"])
     }
-    console.log("USER METADATA", userMetadata)
   }, [userMetadata]);
 
-  const handleUsernameSubmit = async (e) => {
-    e.preventDefault()
-    const response = await fetch('/users/update_user_metadata', {
+  const saveUsername = async (username) => {
+    const response = await fetch('/users/username/save', {
       method: "POST",
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        userId: user.sub,
-        username: usernameInput
+        username: username
       })
     })
+    return response
+  }
+
+  const doesUsernameExist = async (username) => {
+    const response = await fetch(`/users/username/exists/${username}`, {
+      method: "GET",
+    })
     const data = await response.json()
-    setUserMetadata(data.user_metadata)
+    setUsernameExists(data)
+    return data
+  }
+
+  const handleUsernameSubmit = async (e) => {
+    e.preventDefault()
+    const exists = await doesUsernameExist(usernameInput)
+    if (!exists) {
+      saveUsername(usernameInput)
+      const response = await fetch('/users/update_user_metadata', {
+        method: "POST",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          userId: user.sub,
+          username: usernameInput
+        })
+      })
+      const data = await response.json()
+      setUserMetadata(data.user_metadata)
+    }
   }
 
   if (isLoading) {
@@ -57,9 +84,16 @@ const UserSetup = () => {
   //submit username form and Plaid Link
   return (
     <>
-      {isAuthenticated && !userMetadata.username && (
+      {isAuthenticated && !userMetadata.username && !usernameExists && (
         <form className={classes.root} noValidate autoComplete="off" onSubmit={e => { handleUsernameSubmit(e) }}>
-          <TextField id="standard-basic" label="Username" onChange={e => setUsernameInput(e.target.value)} />
+          <TextField id="standard-basic" label="Username" value={usernameInput} onChange={e => setUsernameInput(e.target.value.toLowerCase())} />
+          <br />
+          <Button type="submit" color="primary" variant="contained">Reserve Username</Button>
+        </form>
+      )}
+      {isAuthenticated && !userMetadata.username && usernameExists && (
+        <form className={classes.root} noValidate autoComplete="off" onSubmit={e => { handleUsernameSubmit(e) }}>
+          <TextField error helperText="Username Taken" id="standard-basic" label="Username" value={usernameInput} onChange={e => setUsernameInput(e.target.value.toLowerCase())} />
           <br />
           <Button type="submit" color="primary" variant="contained">Reserve Username</Button>
         </form>
