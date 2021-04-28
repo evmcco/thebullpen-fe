5865860cac42a4c572b687d8c3187c44f8bc821d
Evan McCoy <evan.mccoy@gmail.com>
2021-04-28 09:18:46 -0400

    fixing flash of UC page

--- src/components/Home.js
+++ src/components/Home.js
@@ -31,31 +31,34 @@ const Home = () => {
   }));
 
   const classes = useStyles();
-  return (
-
-    isAuthenticated ?
-      <div className={classes.body} >
-        <div className={classes.heading}>
-          <h1>Bullpen</h1>
-          <h2 className={classes.subheading}>Stampeding toward the Moon since 2021</h2>
-        </div>
-        <div className={classes.root} >
-          <Grid container spacing={3} direction="row" justify="center">
-            {isAuthenticated &&
+  if (isLoading) {
+    return <div className={classes.body} />
+  } else {
+    return (
+      isAuthenticated ?
+        <div className={classes.body} >
+          <div className={classes.heading}>
+            <h1>Bullpen</h1>
+            <h2 className={classes.subheading}>Stampeding toward the Moon since 2021</h2>
+          </div>
+          <div className={classes.root} >
+            <Grid container spacing={3} direction="row" justify="center">
+              {isAuthenticated &&
+                <Grid item xs={12} sm={6}>
+                  <UserGroupsList username={user["https://thebullpen.app/username"]} title={"My Groups"} />
+                </Grid>}
               <Grid item xs={12} sm={6}>
-                <UserGroupsList username={user["https://thebullpen.app/username"]} title={"My Groups"} />
-              </Grid>}
-            <Grid item xs={12} sm={6}>
-              <TrendingBullpens />
+                <TrendingBullpens />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TrendingGroups />
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <TrendingGroups />
-            </Grid>
-          </Grid>
-        </div>
-      </div >
-      : <UnderConstruction />
-  )
+          </div>
+        </div >
+        : <UnderConstruction />
+    )
+  }
 }
 
 
